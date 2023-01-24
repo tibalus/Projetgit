2936f182e29b3a038b67617cd380cc60b0102023
tibal <tibo.roger22@gmail.com>
2023-01-24 10:14:17 +0100

    init mvc

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<div class="container p-5 my-5 border">
+  <h1>ProjetGit - Gestionnaire de Jeu </h1>
+  <p>Ce projet est une révolution dans l'indsutrie du jeu vidéo ! Elle vous permet de stocker tout vos jeux !! Cela est pratique pour les gens atteints de problèmes de mémoire par exemple</p>
+  <a href="connexion.php"><button  type="button" class="btn btn-dark">Connexion</button></a>
+  <a href="inscription.php"><button type="button" class="btn btn-light">Inscription</button></a>
+  
+</div>
+
+
+</body>
+</html>
